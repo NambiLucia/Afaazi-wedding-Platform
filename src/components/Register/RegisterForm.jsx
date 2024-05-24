d83fb2c3d03792645c7d2fee6d13af79a1533415
lucia nambi <nambilucia@gmail.com>
2024-05-24 21:49:26 +0300

    add register information to strapi

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -5,143 +5,139 @@ import "./register.css";
 function RegisterForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); 
   const [contact, setContact] = useState("");
   const [location, setLocation] = useState("");
   const [gender, setGender] = useState("");
   const [dob, setDob] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
 
-  
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (name.length!==0 && email.length!==0  &&
-      contact.length!==0 &&location.length!==0 &&
-      gender.length!==0 && dob.length!==0 ) { 
-       
-  
-      const registerUrl = "http://localhost:1337/api/auth/local/registers";
-      const registerObj = {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({ data: { 
-          name,email,contact,location,gender,dob
-         } }),
-      };
-
-      fetch(registerUrl, registerObj)
-        .then((response) => {
-          if(!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-          return response.json();
-        })
-
-        .then((data) => {
-          console.log("Registration successful", data);
+    if (name && email && password && contact && location && gender && dob) {
+      try {
+        const response = await fetch('http://localhost:1337/api/auth/local/register', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            username: name,
+            email: email,
+            password: password, 
+            contact,
+            location,
+            gender,
+            dob,
+          }),
+        });
+        const data = await response.json();
+        if (response.ok) {
+          alert('Signup successful!');
           setName("");
           setEmail("");
           setContact("");
           setLocation("");
           setGender("");
           setDob("");
-          setErrorMessage('');
-          
-        })
-        .catch((error) => {
-          setErrorMessage("Registration failed: " + error.message);
-          
-        });
-     
-      
+        } else {
+          alert(`Failed to sign up: ${data.message}`);
+        }
+      } catch (error) {
+        console.error('Error signing up:', error);
+        alert('Error signing up');
+      }
     } else {
-      
-      setErrorMessage("All fields are required");
+      alert('Information missing!');
     }
   };
 
   return (
     <section className="register-section">
-    <div className="registration-form">
-      <h1>Registration Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) =>setName(e.target.value)}
-            required
-          />
-          
+      <div className="registration-form">
+        <h1>Registration Form</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Contact Number:</label>
+            <input
+              type="tel"
+              name="contactNumber"
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Location:</label>
+            <input
+              type="text"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label>Gender:</label>
+            <select
+              name="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              required
+            >
+              <option value="">Select Gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Date of Birth:</label>
+            <input
+              type="date"
+              name="dob"
+              value={dob}
+              onChange={(e) => setDob(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">
+            Register
+          </button>
+        </form>
+        <div className="login-link">
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
         </div>
-        <div className="form-group">
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) =>setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Contact Number:</label>
-          <input
-            type="tel"
-            name="contactNumber"
-            value={contact}
-            onChange={(e) =>setContact(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Location:</label>
-          <input
-            type="text"
-            name="location"
-            value={location}
-            onChange={(e) =>setLocation(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Gender:</label>
-          <select
-            name="gender"
-            value={gender}
-            onChange={(e) =>setGender(e.target.value)}
-            required
-          >
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label>Date of Birth:</label>
-          <input
-            type="date"
-            name="dob"
-            value={dob}
-            onChange={(e) =>setDob(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">
-          Register
-        </button>
-      </form>
-      <div className="login-link">
-        <p>
-          Already have an account? <Link to="/login">Login</Link>
-        </p>
       </div>
-
-    </div>
-
     </section>
   );
 }
