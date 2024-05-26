1a07c214a120d78e940042e14f0972c9324d8165
lucia nambi <nambilucia@gmail.com>
2024-05-26 19:48:26 +0300

    add changes to hero page and services for responsiveness and GUI

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -3,27 +3,37 @@ import { Link } from "react-router-dom";
 import "./services.css";
 import NavBar from "../NavBar";
 
-function Services () {
+function Services() {
   const servicesData = [
     {
       image: "/images/photography.png",
-      title: "Photography Services",
+      title: "Photography ",
       description: "Golazo Photography",
     },
     {
       image: "/images/ashton.png",
-      title: "Decoration Services",
+      title: "Decoration ",
       description: "iCandy Ug",
     },
     {
       image: "/images/food.png",
-      title: "Catering Services",
+      title: "Catering ",
       description: "Exceed Catering",
     },
     {
       image: "/images/makeup.png",
-      title: "Make-up Services",
+      title: "Make-Up ",
       description: "Mona Faces",
+    },
+    {
+      image: "/images/janzi.png",
+      title: "Band Musi",
+      description: "Hire A Band",
+    },
+    {
+      image: "/images/icandyUg.png",
+      title: "The Ambiance ",
+      description: "Suitable Place",
     },
     {
       image: "/images/executive.png",
@@ -32,40 +42,37 @@ function Services () {
     },
     {
       image: "/images/ivuga.png",
-      title: "Music / Entertainment Services",
+      title: "Entertainment ",
       description: "Ivuga Band",
-    }
-   
-   
+    },
   ];
 
   return (
-    
     <div className="services-container">
-        <NavBar />
-       
-      <h1 className="title">Afaazi Services</h1>
-      <p className="subtitle">Explore our Services!</p>
-      <div className="card-container">
-        {servicesData.map((service, index) => (
-          <div className="card" key={index}>
-            <img
-              src={service.image}
-              alt={`Event ${index + 1}`}
-              className="card-image"
-            />
-            <div className="card-text">
-              <p className="service-title">{service.title}</p>
-              <p className="service-description">{service.description}</p>
-              <Link to="/gallery" className="registration-button">
-                View more
-              </Link>
+      <NavBar />
+      <div className="content">
+        <h1 className="title">Explore our Services!</h1>
+        <div className="card-container">
+          {servicesData.map((service, index) => (
+            <div className="card" key={index}>
+              <img
+                src={service.image}
+                alt={`Event ${index + 1}`}
+                className="card-image"
+              />
+              <div className="card-text">
+                <h2 className="service-title">{service.title}</h2>
+                <p className="service-description">{service.description}</p>
+                <Link to="/gallery" className="view-more-button">
+                  View more
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Services;
